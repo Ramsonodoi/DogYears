070fad6eca8e847e8005aa365224d94a5686a341
Ramson <ramson.odoi@amalitech.org>
2021-08-31 12:18:11 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// my current age
+const myAge= 29;
+
+//  early years
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+
+// calculating for first two years
+let laterYears = myAge - 2;
+
+// calculating the number of dog years accounted
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//calculate the total age of dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+
+//  convert myname to lowercase
+
+let myName='RAMSON ODOI';
+
+
+console.log(`My Name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
